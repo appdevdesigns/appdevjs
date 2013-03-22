b53fe37892172f31dc597f216a2f2c1152ece739
Brian Zoetewey <brian.zoetewey@ccci.org>
2013-03-22 03:46:46 -0400

    Update install to use node package.json to manage and install server dependencies

--- install/install.sh
+++ install/install.sh
@@ -17,27 +17,8 @@ then
 
     cd ../server
     echo "Installing node modules..."
-    npm install ansi-color
-    npm install ejs
-    npm install express@2.5.9
-    npm install express-params
-    npm install socket.io@0.9.0
-    npm install mysql@2.0.0-alpha3
-	npm install faye
-    npm install jquery-deferred
-    npm install temp
-    npm install eventemitter2
-    npm install async
-    npm install nodeunit
-    npm install request # For node-mandrill
-    npm install underscore # For node-mandrill
-    npm install jsdom # For node-browser
-    npm install mimelib-noiconv
+    npm install
 
-    #npm install cas # <-- this won't work until the module maintainer merges my code on github (Mantis 2272)
-    # http://appdev.appdevdesigns.net/mantis/view.php?id=2272#c8390
-    git clone https://github.com/joshchan/node-cas.git node_modules/cas
-    
     chmod 777 ../web/scripts/js
     
     cd ../install
