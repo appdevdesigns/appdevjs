fe58314aad77d76600903ccdecd941f2c19171bd
Bobby Ratliff <bobby@appdevdesigns.net>
2013-05-13 20:38:04 +0800

    Add tests for the site module
    * Add tests to site_labels_test_mocha
    * Add tests to site_language_test_mocha
    * Skip tests that reference a specific number of labels. These tests
    will need to be rewritten so they can run on any number of different
    setups.

--- modules/site/web/pages/unitTests/tests/site_labels_test_mocha.js
+++ modules/site/web/pages/unitTests/tests/site_labels_test_mocha.js
@@ -1,44 +1,64 @@
+var expect = chai.expect;
 
-	describe('site labels', function(){
-		var label = new site.Labels({});
-		var labelId = 0;
-		 before(function(done){
-			label = new site.Labels({
-				language_code: 'en',
-				label_key: '[testing.add.of.label]',
-				label_label: 'Testing',
-				label_lastMod: '2013-04-03 08:01:01',
-				label_needs_translation: 0,
-				label_path: '/page/site/adminToolbar'
-			});
-			label.save(function(){
-				labelId = label.getID();
-				done();
-			});
-		 })
+describe('site labels', function(){
+    var label = new site.Labels({});
+    var labelId = 0;
+    before(function(done){
+        label = new site.Labels({
+            language_code: 'en',
+            label_key: '[testing.add.of.label]',
+            label_label: 'Testing',
+            label_lastMod: '2013-04-03 08:01:01',
+            label_needs_translation: 0,
+            label_path: '/page/site/adminToolbar'
+        });
+        label.save(function(){
+            labelId = label.getID();
+            done();
+        });
+    });
 
-		it('site labels findOne', function(done){
-		    site.Labels.findOne({label_id:labelId},function(data){
-		    	var findOneId = data.getID();
-		    	chai.expect(findOneId).to.equal(labelId);
-		    	done();
-		  	});
-		 })
+    it('should have a label_key', function() {
+        expect(label.label_key).to.equal('[testing.add.of.label]');
+    });
+    it('should have a language_code', function() {
+        expect(label.language_code).to.equal('en');
+    });
+    it('should have a label_label', function() {
+        expect(label.label_label).to.equal( 'Testing');
+    });
+    it('should have a label_lastMod', function() {
+        expect(label.label_lastMod).to.equal( '2013-04-03 08:01:01');
+    });
+    it('should have a label_needs_translation', function() {
+        expect(label.label_needs_translation).to.equal( 0);
+    });
+    it('should have a label_path', function() {
+        expect(label.label_path).to.equal( '/page/site/adminToolbar');
+    });
 
-		 it('site labels destroy', function(done){
-			 label.destroy(function(data){
-				 site.Labels.findAll({},function(list){
-					 chai.assert.deepEqual(list.length,344);
-					 done();
-				 })
-			 })
-		 })
+    describe('site labels findOne', function(){
+        it('when label_id is specified', function(done) {
+            site.Labels.findOne({label_id:labelId},function(data){
+                var findOneId = data.getID();
+                expect(findOneId).to.equal(labelId);
+                done();
+            });
+        });
+    });
+    it.skip('site labels destroy', function(done){
+        label.destroy(function(data){
+            site.Labels.findAll({},function(list){
+                chai.assert.deepEqual(list.length,344);
+                done();
+            })
+        });
+    });
+    it.skip('site labels findAll', function(done){
+       site.Labels.findAll({},function(list){
+           chai.assert.deepEqual(list.length,344);
+           done();
+       });
+    });
 
-		 it('site labels findAll', function(done){
-		    site.Labels.findAll({},function(list){
-
-		    	chai.assert.deepEqual(list.length,344);
-		    	done();
-		  	});
-		 })
-	})
+});
--- modules/site/web/pages/unitTests/tests/site_language_test_mocha.js
+++ modules/site/web/pages/unitTests/tests/site_language_test_mocha.js
@@ -1,9 +1,42 @@
+var expect = chai.expect;
 
-	describe('site language', function(){
-		 it('site language findAll', function(){
+describe('language', function() {
+    var language;
+    before(function(done){
+        var _done = done;
+        site.Language.findAll({}, function(list){
+            language = list[0];
+            _done();
+        });
+    });
+    it('should have a language_code', function() {
+        expect(language.language_code).to.exist;
+    });
+    it('should have a language_id', function() {
+        expect(language.language_id).to.exist;
+    });
+    it('should have a language_label', function() {
+        expect(language.language_label).to.exist;
+    });
+	describe('preinstalled site languages', function(){
+		it('should be 2 installed', function(){
 		    site.Language.findAll({},function(list){
-console.log(list.length);
 		    	chai.assert.equal(list.length,2);
 		  	});
-		 })
-	})
+		});
+	});
+    describe('site.Language.findAll', function(){
+        it('should return an array', function(){
+            site.Language.findAll({},function(list){
+                expect(list).to.be.instanceof(Array);
+            });
+        });
+    })
+    describe('AD.Lang.getList', function(){
+        it('should return an array', function() {
+            AD.Lang.getList(function(list){
+                expect(list).to.be.instanceof(Array);
+            });
+        });
+    });
+});
